e9edcb1eb668abc83847597e557864b9514dfef7
Natsyu <rainbowvme@gmail.com>
2016-10-26 19:13:36 +0200

    koniec wkładu

--- Kurs_Cis_.NET_polsl/Pierwsze_spotkanie/Program.cs
+++ Kurs_Cis_.NET_polsl/Pierwsze_spotkanie/Program.cs
@@ -6,12 +6,39 @@ using System.Threading.Tasks;
 
 namespace Pierwsze_spotkanie
 {
+    class Dzik
+    {
+        public string name;
+        public Dzik(string nazwa)
+        {
+            name = nazwa;
+        }
+    }
+
+    public class Person
+    {
+        // Propercje
+           public int age { get; set; } // Zapewnia hermetyzacje
+        public string Name { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            string x = "kameha";
+            var Boear = new Dzik("TUTUTUTUTUT");
+
+            var osoba = new Person;
+
+            osoba.age = 5;
+
             Console.Write("Test textu");
+            Console.Write(Boear.name);
             Console.ReadKey();
+            
         }
     }
+
+
+
 }
