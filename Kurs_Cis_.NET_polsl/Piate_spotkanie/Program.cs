88d65558a24aceb518a43b0ceeedd13fcd597f14
Natsyu <rainbowvme@gmail.com>
2016-12-10 15:23:09 +0100

    5 spotkanie C#, winform

---
+++ Kurs_Cis_.NET_polsl/Piate_spotkanie/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+
+namespace Piate_spotkanie
+{
+
+    
+
+
+    class Program
+    {
+        public static void Example1()
+        {
+            //Define and run task
+            Thread.CurrentThread.Name = "Main";
+
+            // Output a message from the calling thread
+            Task taskA = Task.Run(
+                () => Console.WriteLine("Hello from taskA."));
+
+            Console.WriteLine("Hello from thread '{0}'.", Thread.CurrentThread.Name);
+
+
+            Task taskB = new Task(() => Console.WriteLine("Hello from taskB."));
+
+            taskB.Start();
+            taskA.Wait();
+
+            taskB.Wait();
+
+        }
+
+        public static void Example2()
+        {
+            Task<double>[] taskArray = {Task<double>.Factory.StartNew(()=> DoComputation(1.0)),
+                                        Task<double>.Factory.StartNew(()=> DoComputation(100.0)),
+                                        Task<double>.Factory.StartNew(()=> DoComputation(1000.0))};
+
+            var resault = new double[taskArray]
+        }
+
+
+        static void Main(string[] args)
+        {
+            Example1();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Kurs_Cis_.NET_polsl/Piate_spotkanie_WinForm/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Piate_spotkanie_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WriteLog(string message)
+        {
+            listBox1.Items.Insert(0,
+                String.Format("{0} {1} {2}", DateTime.Now.TimeOfDay.ToString(),
+                Thread.CurrentThread.ManagedThreadId,
+                message));
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            WriteLog("Startuje 1");
+            for (int i = 0; i < 20; i++)
+            {
+                WriteLog(i.ToString());
+                await Task.Run(() => Thread.Sleep(1000));
+            }
+            WriteLog("Koniec 1");
+            button2.Enabled = true;
+        }
+    }
+}
